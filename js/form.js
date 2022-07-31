353d3854d713e820b6ffd953fa7a844f5b049578
Sanjithj <sanjithj591997@gmail.com>
2022-07-31 12:13:08 +0530

    Added: Password added

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@ function validation() {
     var text = document.getElementById("text").value;
     var mail = document.getElementById("email").value;
     var phone = document.getElementById("mob").value;
+    var password = document.getElementById("pwd").value;
     
     var textRegex = /"^[A-Z]{1}[a-z]{2,}$"/;
     if (!textRegex.test(text)) {
