bcf9ae9d992c5f41b439ff830a8addd75166ca0e
Sanjithj <sanjithj591997@gmail.com>
2022-07-31 12:01:11 +0530

    Added: Email added

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 function validation() {
     var text = document.getElementById("text").value;
-   
+    var mail = document.getElementById("email").value;
+    
     var textRegex = /"^[A-Z]{1}[a-z]{2,}$"/;
     if (!textRegex.test(text)) {
         alert("Invalid Name format."+" Should contain first upperCase, lowerCase and length >= 3");
