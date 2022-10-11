41832ad1bf635bbe354dbca7074adcd869875e73
Sanjithj <104911818+Sanjithj@users.noreply.github.com>
2022-10-11 16:51:20 +0530

    JS.form

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 function validation() {
     var text = document.getElementById("text").value;
-   
     var textRegex = /"^[A-Z]{1}[a-z]{2,}$"/;
     if (!textRegex.test(text)) {
         alert("Invalid Name format."+" Should contain first upperCase, lowerCase and length >= 3");
